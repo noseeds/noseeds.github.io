ea01eec77a8cb695e3f598d322989e4f5db9474e
Elias Suárez <113398356+noseeds@users.noreply.github.com>
2024-07-25 13:56:34 -0300

    implementación

---
+++ js/login.js
@@ -0,0 +1,32 @@
+function obtenerVariableGET(nombre, url = window.location.href) {
+    // obtiene la parte de la url donde se ubican las variables get
+    const variablesGET = window.location.search;
+    // crea un objeto que contiene cada variable get por separado
+    const urlParams = new URLSearchParams(variablesGET);
+    // se agarra el valor de la variable especifica
+    const valor = urlParams.get("formularioActual");
+    return(valor);
+}
+
+$(document).ready(function () {
+    var loginForm = $("#login");
+    var registroForm = $("#registro");
+    //cambiar de formulario
+    $(".ingreso_o_registro").click(function (event) {
+        if (loginForm.css("display") === "none") {
+            loginForm.css("display", "flex");
+            registroForm.css("display", "none");
+        } else {
+            loginForm.css("display", "none");
+            registroForm.css("display", "flex");
+        }
+    });
+    //mostrar el formulario correcto y ocultar el otro
+    if (obtenerVariableGET("formularioActual") === "registro") {
+        loginForm.css("display", "none");
+        registroForm.css("display", "flex");
+    } else {
+        loginForm.css("display", "flex");
+        registroForm.css("display", "none");
+    }
+});  
